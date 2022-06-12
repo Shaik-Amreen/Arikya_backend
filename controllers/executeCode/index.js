66a0ae59ae0cc68e45a23a48cab9f6be168eaa68
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-06-12 10:28:35 +0530

    Revert "branch from arikya backend devops"
    This reverts commit 2f0f62aa88e5566e503690ab2d4dbcd4518f142c.

--- controllers/executeCode/index.js
+++
@@ -1,9 +0,0 @@
-const { executeJava } = require("./java.js");
-const { executePython } = require("./python.js");
-const { executeCorCPP } = require("./c_or_cpp.js");
-
-module.exports = {
-  executeJava,
-  executePython,
-  executeCorCPP,
-};
--- models/mail.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const Schema = new mongoose.Schema({
-  to: { type: "string", requqired: true },
-  subject: { type: "string", required: true },
-  content: { type: "string", required: true },
-  organisation_id: { type: "string" },
-})
-const mailcollection = mongoose.model("mails", Schema)
-module.exports = mailcollection
